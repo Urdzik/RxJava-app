775c0a2a7e368394b62273d9cb379d2badb7b918
Slavik Urdzik <Urdziksv@gmail.com>
2020-03-23 10:19:19 +0200

    Lesson 2

--- app/src/main/java/com/example/beginapplication/MainActivity.kt
+++ app/src/main/java/com/example/beginapplication/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.core.*
 import io.reactivex.rxjava3.schedulers.Schedulers
+
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,29 +16,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       dataSource()
-           .subscribeOn(Schedulers.newThread())
-           .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                {
+
+       dataSsource()
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({list ->
+                list.forEach {
                     text.text = it.toString()
-                },
-                {
-
-                }, {
-                    Log.e("tag", "finish")
-                })
-
-        }
-
+                }
+            },{
+                Log.e("tag", "$it")
+            })
     }
 
-    private fun dataSource(): Observable<Int> = Observable.create { subscriber ->
-        for (i in 1..100) {
-            Thread.sleep(100)
-            subscriber.onNext(i)
 
+    private fun dataSource(): Observable<Int> {
+        return Observable.create { subscriber ->
+            for (i in 1..10000000) {
+
+                subscriber.onNext(i)
+            }
+
+            subscriber.onComplete()
         }
-
     }
 
+    private fun dataSsource(): Single<List<Int>> {
+        return Single.create { subscriber ->
+           val list = listOf(1,2,3,4,5,6,7,8)
+            subscriber.onSuccess(list)
+        }
+    }
+}
