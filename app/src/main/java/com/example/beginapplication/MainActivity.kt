c3b356aaf83ef6853564be3f9b6f91b84b2bff20
Slavik Urdzik <Urdziksv@gmail.com>
2020-03-24 10:53:06 +0200

    flatMapIterable

--- app/src/main/java/com/example/beginapplication/MainActivity.kt
+++ app/src/main/java/com/example/beginapplication/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.beginapplication
 
-
 import android.os.Bundle
-import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
-import java.util.concurrent.TimeUnit
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 private val disposeBag = CompositeDisposable()
@@ -15,28 +15,36 @@ private val disposeBag = CompositeDisposable()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val result1 = Observable.just("1", "2", "3", "4", "5")
-            .delay(1, TimeUnit.SECONDS)
+        val list: List<String> = listOf("one", "two", "three", "four", "five")
+
+        val dispose = Observable.just(list)
+            .flatMapIterable { it }
+            .scan { t1: String?, t2: String? -> "$t1, $t2"}
             .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                println(it)
-            },{
-                println(it)
+                text.text = it
+            }, {
+
             })
 
-        val result2 = Observable.just("1", "2", "3", "4", "5")
-            .delay(1, TimeUnit.SECONDS)
-            .subscribeOn(Schedulers.newThread())
-            .subscribe({
-                println(it)
-            },{
-                println(it)
-            })
-        disposeBag.addAll(result1, result2)
 
-        Handler().postDelayed({
-            println("Disposable")
-            disposeBag.dispose()
-        }, 2000)
+//        val dispose = Observable.just("Алексей", "Владимир", "Грегорий", "Дмитрий", "Евгений")
+//            .scan { t1: String, t2: String -> "$t1 $t2" }
+//            .subscribeOn(Schedulers.newThread())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe({
+//                println(it)
+//            },{
+//
+//            })
+//
+//
+//        disposeBag.addAll(dispose)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposeBag.clear()
     }
 }
